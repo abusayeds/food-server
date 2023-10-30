babad50308dfcd773ec0f72a4e1bed237f7b6fae
abusayeds <abusayedstudent855@gmail.com>
2023-10-30 19:23:12 +0600

    create get

--- index.jsx
+++ index.jsx
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.port || 5000
 
-const reviewrestaurants = require('./data/food.json')
+const reviewrestaurants = require('./data/restaurants.json')
+const foods = require('./data/foods.json')
+ app.use(cors())
+
 
 app.get('/', (req, res) => {
     res.send('API runing')
@@ -10,6 +14,12 @@ app.get('/', (req, res) => {
 
   app.get('/food-restaurants' , (req , res ) => {
     res.send(reviewrestaurants)
+  })
+
+  app.get('/foods/:id' , (req , res) => {
+    const id =req.params.id 
+    const foodsid = foods.find( food => food._id === id)
+    res.send(foodsid)
   })
   
   app.listen(port, () => {
