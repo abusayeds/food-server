d3f9948e7425b197d2c131754b5125cfb814409a
abusayeds <abusayedstudent855@gmail.com>
2023-11-02 11:15:15 +0600

    category api date set up

--- index.jsx
+++ index.jsx
@@ -5,6 +5,7 @@ const port = process.env.port || 5000
 
 const reviewrestaurants = require('./data/restaurants.json')
 const foods = require('./data/foods.json')
+const items = require('./data/items.json')
  app.use(cors())
 
 
@@ -20,6 +21,16 @@ app.get('/', (req, res) => {
     const id =req.params.id 
     const foodsid = foods.find( food => food._id === id)
     res.send(foodsid)
+  })
+
+  // app.get('/items/:id', (req , res) => {
+  //   const id =req.params.id 
+  //   const itemId  = foods.find(food => food.items.items_id === id )
+  //   res.send(itemId)
+  // })
+
+  app.get('/items' , (req , res) => {
+    res.send(items)
   })
   
   app.listen(port, () => {
