257dd9e4c21f1e3bec21e7d5dc9d3ddee9fafd34
abusayeds <abusayedstudent855@gmail.com>
2024-01-15 23:14:06 +0600

    servise data added

--- index.jsx
+++ index.jsx
@@ -3,11 +3,17 @@ const app = express()
 const cors = require('cors')
 const port = process.env.port || 5000
 
+require('dotenv').config()
+
+app.use(cors())
+app.use(express.json())
+
+
+
 const reviewrestaurants = require('./data/restaurants.json')
 const foods = require('./data/foods.json')
-const items = require('./data/items.json')
- app.use(cors())
-
+const servises = require ('./data/Servises.json')
+ const checout = require('./data/Checout.json')
 
 app.get('/', (req, res) => {
     res.send('API runing')
@@ -16,18 +22,27 @@ app.get('/', (req, res) => {
   app.get('/food-restaurants' , (req , res ) => {
     res.send(reviewrestaurants)
   })
-
+  app.get('/servises', (req, res) => {
+    res.send(servises)
+  })
   app.get('/foods/:id' , (req , res) => {
     const id =req.params.id 
     const foodsid = foods.find( food => food._id === id)
     res.send(foodsid)
   })
+ app.get('/checout/:id', (req, res) => {
+  const id =req.params.id
+  const ChecoutId=checout.find( checkid => checkid._id === id)
+  res.send(ChecoutId) 
+ })
+  
 
-  // app.get('/items/:id', (req , res) => {
-  //   const id =req.params.id 
-  //   const itemId  = foods.find(food => food.items.items_id === id )
-  //   res.send(itemId)
-  // })
+  
+
+
+
+
+  
 
   app.get('/items' , (req , res) => {
     res.send(items)
